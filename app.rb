31b4ae711a93ce5368b6599402f0967fa5bcd3ed
djevrem <djevrem@gmail.com>
2015-11-08 03:53:16 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -40,6 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    res = @game.guess(letter)	
+    if res == false
+	flash[:message]="You have already used that letter."
+    else
+	flash[:message]="Invalid guess."
+    end	
     redirect '/show'
   end
   
@@ -50,12 +56,16 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :play
+    	erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+    	erb :win # You may change/remove this line
+    end
   end
   
   get '/lose' do
